9732d13d19a0de8e90643c73d8d366278937a692
Gus <g.sosacuervo1@gmail.com>
2019-06-22 14:31:27 -0500

    Adding the code

---
+++ NancyConsoleServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using CommandLine;
+using Nancy;
+using Nancy.Hosting.Self;
+using System;
+
+namespace NancyStandalone
+{
+    class Program
+    {
+        static void Main(string[] args) =>
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    var hostConfiguration = new HostConfiguration();
+                    var url = $"http://{o.Url}:{o.Port}";
+                    hostConfiguration.UrlReservations.CreateAutomatically = true;
+                    using (var host = new NancyHost(hostConfiguration, new Uri(url)))
+                    {
+                        host.Start();
+
+                        Console.WriteLine("NancyFX Stand alone test application.");
+                        Console.WriteLine($"Server running on {url}");
+                        Console.WriteLine("Press enter to exit the application");
+                        Console.ReadLine();
+                    }
+                });
+    }
+
+    class Options
+    {
+        [Option('u', "url", Required = false, HelpText = "Url to use. Default is localhost.", Default = "localhost")]
+        public string Url { get; set; }
+
+        [Option('p', "port", Required = false, HelpText = "Port to use. Default is 80", Default = 80)]
+        public int Port { get; set; }
+    }
+}
+
+public class HelloModule : NancyModule
+{
+    public HelloModule()
+    {
+        Get("/", _ => "Hello World");
+    }
+}
